973f90b1cd9899a9d58139b6ea7a560f8490db41
tech3br <dfsdireito95@gmail.com>
2021-01-31 00:30:45 -0300

    Fixing ident at Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,21 @@
 import * as S from './styles'
 
 const Main = ({
-    title = 'React Avancado',
-    description = 'TypeScript, ReactJS, NextJS e Styled Components'
+  title = 'React Avancado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
-    <S.Wrapper>
-        <S.Logo
-            src="/img/reactjs-icon.svg"
-            alt="Imagem de um atomo e React Avançado escrito ao lado"
-        ></S.Logo>
-        <S.Title>{title}</S.Title>
-        <S.Description>{description}</S.Description>
-        <S.Ilustration
-            src="/img/progressive_app.svg"
-            alt="desenvolvedor de frente para tela com codigo"
-        />
-    </S.Wrapper>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/reactjs-icon.svg"
+      alt="Imagem de um atomo e React Avançado escrito ao lado"
+    ></S.Logo>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Ilustration
+      src="/img/progressive_app.svg"
+      alt="desenvolvedor de frente para tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-    it('should render the heading', () => {
-        render(<Main />)
-    })
+  it('should render the heading', () => {
+    render(<Main />)
+  })
 })
