eb610deaff691a4e1cd94a38d47658b5d96fa6b1
tech3br <dfsdireito95@gmail.com>
2021-02-17 01:24:34 -0300

    Adding MediaMatch and Button components with tests

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Desktop: Story = () => (
+  <MediaMatch greatherThan="medium">Only on Desktop</MediaMatch>
+)
+export const Mobile: Story = () => (
+  <MediaMatch lessThan="medium">Only on Mobile</MediaMatch>
+)
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
