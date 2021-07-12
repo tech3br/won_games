9a6ce5d40ca20a02374a6c80bfd5320313c3613b
tech3br <dfsdireito95@gmail.com>
2021-07-12 16:29:52 -0300

    fix: change button component as link

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -47,6 +47,10 @@ export const Wrapper = styled.button<WrapperProps>`
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
 
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #f062c0 50%);
+    }
+
     ${!!size && wrapperModifiers[size](theme)};
     ${!!fullWidth && wrapperModifiers.fullWidth()};
     ${!!hasIcon && wrapperModifiers.withIcon(theme)};
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -55,4 +55,17 @@ describe('<Button />', () => {
     expect(screen.getByText(/comprar agora/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+
+  it('should render Button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
