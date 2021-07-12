23ff65adeaed22f12d9ace7d44998b61308c43f2
tech3br <dfsdireito95@gmail.com>
2021-07-12 17:27:23 -0300

    fix: change button component stories as link

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -25,6 +25,15 @@ export const withIcon: Story = (args) => <Button {...args} />
 
 withIcon.args = {
   size: 'small',
-  children: 'Comprar Agora',
+  children: 'Buy now',
   icon: <AddShoppingCart />
+}
+
+export const asLink: Story = (args) => <Button {...args} />
+
+asLink.args = {
+  size: 'large',
+  children: 'Buy now',
+  as: 'a',
+  href: '/link'
 }
