42122710dd6af8880922c3f6b10819557ccd9ae5
tech3br <dfsdireito95@gmail.com>
2021-07-12 22:33:57 -0300

    feat: adding banner component and tests

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/ })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/ })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/ })).toBeInTheDocument()
+  })
+})
