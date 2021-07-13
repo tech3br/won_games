9b5ced65c2627988f6694a467ffd508c5597a0c2
tech3br <dfsdireito95@gmail.com>
2021-07-13 12:57:41 -0300

    feat: Adding highlight component and structure

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead its back',
+    subtitle: 'Come see Johns new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
