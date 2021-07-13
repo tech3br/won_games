0421606b23f903c54b7de1b7881c851e8fbf7c83
tech3br <dfsdireito95@gmail.com>
2021-07-12 23:37:21 -0300

    fix: adding snapshot to banner component and adding media queries

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -14,7 +14,7 @@ const props = {
 
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Banner {...props} />)
+    const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Defy death/ })
@@ -25,5 +25,7 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /Defy death/ })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
